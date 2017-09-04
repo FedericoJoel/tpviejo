39af0d043f0e1ad0876469e8cca3034d5e10e600
FedericoJoel <fechhe@gmail.com>
2017-09-03 23:56:15 -0300

    first push

---
+++ yamaLib/lib.c
@@ -0,0 +1,6 @@
+#include "lib.h"  /* Include the header (not strictly necessary here) */
+
+void saludar()    /* Function definition */
+{
+    puts("hola");
+}
---
+++ yamaLib/lib.h
@@ -0,0 +1,13 @@
+/*
+ * lib.h
+ *
+ *  Created on: 2/9/2017
+ *      Author: utnso
+ */
+
+#ifndef LIB_H_
+#define LIB_H_
+
+void saludar();
+
+#endif /* LIB_H_ */
