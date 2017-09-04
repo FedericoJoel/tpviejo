15e01fb3bc58e673a00c3eec61b6487b8ec1f9e5
leandroFernandez94 <leandroofernandezz@gmail.com>
2017-09-04 09:00:35 -0300

    delete ignored files

---
+++ master/src/master.c
@@ -0,0 +1,24 @@
+/*
+ ============================================================================
+ Name        : master.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <commons/log.h>
+
+int main(void) {
+	t_log* logger;
+	t_log_level level=LOG_LEVEL_INFO;
+
+	logger = log_create("master_log","MASTER",1,level);
+
+	log_info(logger, "starting...");
+
+	return EXIT_SUCCESS;
+}
