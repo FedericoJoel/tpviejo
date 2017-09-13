05ca193d5473ea7c15c9f70c7c1296d4b821ba1a
leandroFernandez94 <leandroofernandezz@gmail.com>
2017-09-13 18:21:32 -0300

    master - abrir archivo

---
+++ master/src/master.h
@@ -0,0 +1,23 @@
+/*
+ * master.h
+ *
+ *  Created on: 13/9/2017
+ *      Author: utnso
+ */
+
+#ifndef MASTER_H_
+#define MASTER_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <commons/log.h>
+#include <commons/string.h>
+
+t_log_level level=LOG_LEVEL_INFO;
+t_log* logger;
+
+t_log* start_logger();
+int abrir_file_args(int argc, char** argv);
+
+
+#endif /* MASTER_H_ */
