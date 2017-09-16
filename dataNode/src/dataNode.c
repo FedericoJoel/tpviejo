1733e2b056ebd655b10d39767dfe28f888a6dfbb
FedericoJoel <fechhe@gmail.com>
2017-09-16 12:31:23 -0300

    agrego multithreading

--- dataNode/src/dataNode.c
+++ dataNode/src/dataNode.c
@@ -11,13 +11,34 @@
 #include "dataNode.h"
 
 int PUERTO_FS=3490;
-
+int s_filesystem;
 
 int main(void) {
+
+	conectarse_fs();
+	while(1){
+		escuchar_fs();
+	}
+
+	return EXIT_SUCCESS;
+}
+
+void conectarse_fs(){
 	char * IP = string_new();
 	string_append(&IP, "127.0.0.1");
 	printf("Intento conectarme a Filesystem\n");
-	int s_filesystem = conectar(PUERTO_FS,IP);
+	s_filesystem = conectar(PUERTO_FS,IP);
 	printf("Conectado a fs con socket %d \n",s_filesystem);
-	return EXIT_SUCCESS;
+}
+
+void set_bloque(){
+	char* mensaje = esperarMensaje(s_filesystem);
+	printf("escribo bloque con los datos %s \n",mensaje);
+}
+
+void escuchar_fs(){
+	if(recibirProtocolo(s_filesystem) == DN_SETBLOQUE){
+		set_bloque();
+	}
+	//TODO hace falta que cree un thread por cada solicitud de escritura? o mientras esta escribiendo algo no puede recivir otra solicitud de escritura?
 }
--- dataNode/src/dataNode.h
+++ dataNode/src/dataNode.h
@@ -15,4 +15,23 @@
 #include <commons/string.h>
 #include <string.h>
 
+
+/////////////
+//PROTOCOLO//
+/////////////
+///FS - DN///
+#define DN_SETBLOQUE 0001
+#define DN_SETBLOQUEANSW 2
+#define DN_GETBLOQUE 3
+#define DN_GETBLOQUEANSW 4
+//FS - YAMA//
+#define FS_READ 5
+#define FS_STORE 6
+#define YM_READANSW 7
+#define YM_STOREANSW 8
+
+void escuchar_fs();
+void set_bloque();
+void conectarse_fs();
+
 #endif /* DATANODE_H_ */
